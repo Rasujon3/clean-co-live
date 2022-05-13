82a36ebd916aaa355eb028853f5509ca3d7351fe
Rasujon3 <rasujon3@gmail.com>
2022-05-13 21:17:08 +0600

    animation add

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -7,16 +7,41 @@ const Landing = () => {
       <div class="hero h-screen lg:h-[60vh] bg-accent z-0 mt-16">
         <div class="hero-content flex-col lg:flex-row">
           <div>
-            <p className="text-xl">Best Quality</p>
-            <h1 class="text-5xl font-bold">Professional Cleaning Service</h1>
-            <p class="py-6 max-w-xl">
+            <p
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-delay="200"
+              className="text-xl"
+            >
+              Best Quality
+            </p>
+            <h1
+              data-aos="fade-right"
+              data-aos-duration="900"
+              data-aos-delay="400"
+              class="text-5xl font-bold"
+            >
+              Professional Cleaning Service
+            </h1>
+            <p
+              data-aos="fade-right"
+              data-aos-duration="800"
+              data-aos-delay="600"
+              class="py-6 max-w-xl"
+            >
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Asperiores, quo?
             </p>
-            <button class="btn btn-primary">Get Started</button>
+            <button
+              data-aos="zoom-in"
+              data-aos-delay="1300"
+              class="btn btn-primary"
+            >
+              Get Started
+            </button>
           </div>
           <div className="h-[60vh] shrink-0">
-            <img src={BuckertGirl} alt="" class="h-full" />
+            <img data-aos="fade-left" src={BuckertGirl} alt="" class="h-full" />
           </div>
         </div>
       </div>
