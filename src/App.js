c793517586e9ec2a2e8bf800e1a287e3b7d95238
Rasujon3 <rasujon3@gmail.com>
2022-05-13 19:56:55 +0600

    routing with seperate component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import Navbar from "./components/Navbar";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
+
 import { Route, Routes } from "react-router-dom";
+import { publicRoute } from "./routes/publicRoutes";
 
 function App() {
   return (
     <>
       <Navbar>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
+          {publicRoute.map(({ path, Component }, index) => (
+            <Route key={index} path={path} element={<Component />} />
+          ))}
         </Routes>
       </Navbar>
     </>
