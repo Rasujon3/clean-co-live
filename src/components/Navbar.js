6ab89f40b6bcc5f69357dac201ca834ade788b87
Rasujon3 <rasujon3@gmail.com>
2022-05-16 19:28:41 +0600

    dark theme bug solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = ({ children }) => {
+  const [dark, setDark] = useState(false);
   return (
-    <div class="drawer drawer-end">
+    <div class="drawer drawer-end" data-theme={dark ? "dark" : "light"}>
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
         <div class="w-full navbar bg-base-100 fixed top-0 z-50 lg:px-20">
@@ -73,7 +74,7 @@ const Navbar = ({ children }) => {
                 </ul>
               </li>
               <label class="swap swap-rotate">
-                <input data-toggle-theme="dark,light" type="checkbox" />
+                <input type="checkbox" onClick={() => setDark(!dark)} />
 
                 <svg
                   class="swap-on fill-current w-10 h-10"
