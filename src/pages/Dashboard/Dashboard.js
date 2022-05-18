5eee8dcb5be267846171cdbbed7e4ce63d92336b
Rasujon3 <rasujon3@gmail.com>
2022-05-18 20:48:55 +0600

    dashboard route - 01:18:28 mins

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,17 @@
 import React from "react";
+import DashboardSidebar from "./DashboardSidebar";
+import { Route, Routes } from "react-router-dom";
+import AddAdmin from "./AddAdmin";
+import AddService from "./AddService";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>This is dashboard</h1>
-    </div>
+    <DashboardSidebar>
+      <Routes>
+        <Route path="/dashboard/ad-admin" element={<AddAdmin />} />
+        <Route path="/dashboard/ad-service" element={<AddService />} />
+      </Routes>
+    </DashboardSidebar>
   );
 };
 
