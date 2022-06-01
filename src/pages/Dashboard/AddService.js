4ba78627fe615bf1c79de6b929d984e38b603bf7
Rasujon3 <rasujon3@gmail.com>
2022-06-02 02:46:06 +0600

    add service done

--- src/pages/Dashboard/AddService.js
+++ src/pages/Dashboard/AddService.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import fetcher from "./../../api/index";
 
 const AddService = () => {
+  const { register, handleSubmit, reset } = useForm();
+
+  const onSubmit = async (data) => {
+    const res = await fetcher.post("add-service", data);
+    console.log(res);
+    reset();
+  };
   return (
-    <div>
-      <h2>Add Service</h2>
+    <div className="flex w-full h-screen bg-accent justify-center items-center">
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <h1 className="text-2xl text-center">Add Service</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Service Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Service Name"
+                class="input input-bordered"
+                {...register("serviceName")}
+              />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Service Charge</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Service Charge"
+                class="input input-bordered"
+                {...register("serviceCharge")}
+              />
+            </div>
+            <div class="form-control mt-6">
+              <button type="submit" class="btn btn-primary">
+                Add Service
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
